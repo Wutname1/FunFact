3a5ee8b27a4724f770167f07e2490149b74abe81
wutname1 <jeremy12@gmail.com>
2022-04-15 18:24:45 -0500

    28 WoW Facts

---
+++ Facts/WoW.lua
@@ -0,0 +1,33 @@
+local FunFact = FunFact
+local module = FunFact:NewModule('FactList_WoW')
+module.desciption = 'Facts about this game called World of Warcraft'
+module.facts = {
+	'Prehistoric hunter-gatherer tribes had an average group limit of 50 people. Incidentally, modern research shows that maximum group player cohesiveness occurs at a limit of 50 players.',
+	'Colleen Lachowicz during her run for state Senate in Maine came under political attack for having played WoW and therefore being out-of-touch with reality and living in a "fantasy world." Colleen ending up winning the Senate seat.',
+	'Yugioh has made more money than X-Men, Lord of The Rings, James Bond, Call of Duty, Superman, Star Trek, WoW, or League of Legends.',
+	'The United States Air Force Pararescue (PJs) use the sound bite of "Leeroy Jenkins" from WoW to notify themselves of an upcoming medevac mission.',
+	'When Uwe Boll tried to obtain filming rights for a WoW movie Blizzard said "We will not sell the movie rights, not to you…especially not to you."',
+	"'Red Shirt Guy' who stumped a panel of WoW devs during a Q&A at BlizzCon, 2010, after asking about an inconsistency in the lore. Later, an NPC was introduced into the game who wore a bright red shirt and was named Wildhammer Fact Checker.",
+	'When Uwe Boll made a bid to direct the Warcraft movie, Blizzard responded, "We will not sell the movie rights, not to you… especially not to you"',
+	'Ben Schulz (more infamously known as Leeroy Jenkins) had a written role in the script for the Warcraft movie. Pvt Jenkins was to break rank "with a zealous battlecry" and be immediately cut down by orcs. The part was ultimately cut from the movie.',
+	'The author of "The Martian" was also a computer programmer for Warcraft 2',
+	'Star Wars Knights of the Old Republic II initially sold more copies than WoW, which released at the same time.',
+	"Trey Parker hated the episode 'Make Love, Not Warcraft' so much, he tried getting the executive producer to pull the show from airing. The episode is regarded as one of the best in the series.",
+	'Robert Hohman quit his job to play WoW full-time for a year before going on to co-found and run Glassdoor.',
+	'Chinese prisoners are forced to farm WoW gold - there are thought to be 100,000 full-time gold farmers in the country.',
+	'By 2012 the players of WoW had collectively spent about 6 million years worth of time playing the game, about as much time as humanity has spent evolving as a species',
+	"A 'virtual virus' was released into WoW that infected players and spread throughout the whole universe. The behaviour of the online players was so lifelike that the incident was studied by scientist to prepare for a real life Pandemic.",
+	'A 12-yr old Norwegian boy managed to survive a moose attack using tactics learned from playing WoW',
+	'A boy in China committed suicide by jumping off of a 24-storey building after a 36-hour video game marathon. His suicide note told about his addiction to Warcraft III and his intent to join his fellow players in heaven.',
+	'The director of Moon, Warcraft and the recently released Muted is the only son of the legendary singer David Bowie',
+	'Greg Street who earned a PhD in marine science, he latter became the lead designer of WoW.',
+	"The director of 'Warcraft' is David Bowie's son.",
+	'Collectively speaking, humans have spent longer playing WoW than they have existed, as a species separate from chimpanzees (5.93 million years).',
+	'Robin Williams was a self-described hardcore gamer and enjoyed playing Warcraft. After his death, Blizzard created an NPC in WoW to commemorate him.',
+	'Daler Mehndi, the pop singer that inspired the WoW Draenei dance, has been accused of human trafficking.',
+	'in 2005 Corrupted Blood caused players to catch a disease from nearby infected, which spread so rapidly that the "major towns and cities were abandoned, leaving urban areas filled to the brim with corpses, and the city streets white with the bones of the dead.',
+	'The CDC uses the "Corrupted Blood" incident in WoW as a study aid on bioterrorism.',
+	'Duncan Jones, Warcraft director and son of David Bowie, graduated from College of Wooster in Ohio.',
+	"Warcraft-based song called 'Loktar Ogar' by revered black metal band 'Rotting Christ' - all lyrics are actual orcish.",
+	"Henry Cavill didn't answer a call from Zack Snyder, who was calling to let him know he got the part for Man of Steel, because he was too busy playing WoW."
+}
