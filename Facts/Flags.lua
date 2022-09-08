864b3f206cd598fe1f13526bdcfc28f93cf7fc54
wutname1 <jeremy12@gmail.com>
2022-09-07 23:39:28 -0500

    Flag facts

---
+++ Facts/Flags.lua
@@ -0,0 +1,22 @@
+local FunFact = FunFact
+local module = FunFact:NewModule('FactList_Flags')
+module.desciption = 'Fun with Flags, sheldon cooper would be proud.'
+
+module.Facts = {
+	'Lichtenstein and Haiti had the same flag and only found out at the 1936 Olympics.',
+	"Nepal has the only flag in the world that doesn't have 4 sides.",
+	'There are only two square shaped flags in the world',
+	'Symbols used on flags normally have a specific meaning',
+	'Japan is known as the “Land of the rising sun”, and therefore they have a circular shape in the middle of their flag which represents the sun.',
+	'The Cross - This appears on many flags and often links back to religion, to represent Christianity',
+	'Some countries will use their national bird on their flags to reflect the countries heritage, like the flag of Ecuador which has a bird of the Andes called a Condor.',
+	'The colors used for national flags often ties into the history of the country.',
+	'Purple is the rarest color to be used on a flag.',
+	'The flag of Belize has the most colors of any flag in the world',
+	'Denmark has the oldest flag in the world. First used in 1625 and is still the same today. The design itself however was created in 1219.',
+	'South Sudan has the newest flag in the world it was adopted on July 9, 2010.',
+	'There are 4 national flags in the world that feature firearms.',
+	'Many will have items such as axes, arrows, cannons, swords, daggers, shields, spears, etc. in their emblems or crests, but only 4 have firearms.',
+	'There have been 27 versions of the American Flag.',
+	'The US Flag designer 17-year-old, Robert G. Heft, of Lancaster, Ohio who submitted the design in a contest in 1958. It was chosen out of over 1,500 submissions by President Dwight Eisenhower.'
+}
